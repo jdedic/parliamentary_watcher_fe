7326f2db07885debea135f04d858806eaa4b2259
Jelena <jelena.dedic@inform-technologies.com>
2026-06-11 16:23:00 +0200

    Update production env variable

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR_BACKEND_RAILWAY_URL/api/v1',
+  apiUrl: 'https://parliamentarywatcherbe-production.up.railway.app/api/v1',
   apiKey: '',
 };
